b03322ba56e132f92d731e7b639624bde993dfb4
EliseuNascimento <60950184+EliseuNascimento@users.noreply.github.com>
2020-03-03 22:12:55 -0300

    Add files via upload

---
+++ 120.py
@@ -0,0 +1,11 @@
+#1ds3
+#Eliseu
+#Feito 03/03/2020
+#Intistuto: ETEC de Itanhaem
+
+x = 0 #declaração de comando
+valor = 2 #declaraçao de variavel de valor
+while = (X<=21):
+		valor = valor * valor
+		x = x + 1 #exibir laço repetição
+print (valor)#exibir varivel valor
